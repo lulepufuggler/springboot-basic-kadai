1c066fc93a49a4e27b8feda7affd831733aafec5
lulepufuggler <misarinmii@gmail.com>
2025-07-23 20:00:36 +0900

    spring_kadai_formをアップロード

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,5 +1,6 @@
 package com.example.springkadaiform.controller;
 
+import org.springframework.core.Conventions;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.springkadaiform.form.ContactForm;
 
@@ -20,21 +22,30 @@ public class ContactFormController {
         return "contactFormView";
     }
     
+    
     // 入力確認画面
     @PostMapping("/confirm")
     public String confirm(
-            @Validated ContactForm form,BindingResult result) {
-
+            @Validated ContactForm form, 
+            BindingResult result,
+            RedirectAttributes redirectAttributes) {
         
         if (result.hasErrors()) {
-        	return "contactFormView";
-        	
-        }
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.contactForm", result);
+            redirectAttributes.addFlashAttribute("contactForm", form);
+            return "redirect:/form";
+            
 
          // バリデーションOKの場合は、確認画面に遷移
+           model.addAttribute("confirm", form);
             return "confirmView"; 
+            
         }
+    }
+    
+}
+    
         
-            }
+
     
     
