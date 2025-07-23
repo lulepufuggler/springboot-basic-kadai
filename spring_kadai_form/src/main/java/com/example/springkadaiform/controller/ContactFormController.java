a308b63f1f3de5c5688f82ff04603e0489a1c5dd
lulepufuggler <misarinmii@gmail.com>
2025-07-23 18:27:50 +0900

    spring_kadai_formをアップロード

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,7 +1,10 @@
 package com.example.springkadaiform.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,14 +16,25 @@ public class ContactFormController {
 	 // お問い合わせフォーム
     @GetMapping("/form")
     public String form(Model model) {
-        model.addAttribute("form", new ContactForm());
+        model.addAttribute("contactform", new ContactForm());
         return "contactFormView";
     }
     
     // 入力確認画面
     @PostMapping("/confirm")
-    public String confirm(ContactForm form, Model model) {
-        model.addAttribute("form", form);
-        return "confirmView";
-    }
-}
+    public String confirm(
+            @Validated ContactForm form,BindingResult result) {
+
+        
+        if (result.hasErrors()) {
+        	return "contactFormView";
+        	
+        }
+
+         // バリデーションOKの場合は、確認画面に遷移
+            return "confirmView"; 
+        }
+        
+            }
+    
+    
